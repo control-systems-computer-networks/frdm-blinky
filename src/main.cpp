e8102a3b4e8fece5bf3f515e7f82cc393ed6dac4
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2017-11-06 16:02:31 +0000

    LED class placeholder

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,37 @@
 #include <mbed.h>
 
-DigitalOut red(LED1);
-DigitalOut green(LED2);
-DigitalOut blue(LED3);
+//DigitalOut red(LED1);
+//DigitalOut green(LED2);
+//DigitalOut blue(LED3);
 
+
+class LED {
+    DigitalOut pin;
+public:
+    LED(PinName p) : pin(p) {off();}
+    void on (void) { pin.write(0);}
+    void off(void) { pin.write(1);}
+};
 enum {on,off};
 
 int main() {
+    LED lights[] = {
+         LED(LED1),LED(LED2),LED(LED3),
+         LED(D5),LED(D8),LED(D9)
+     };
+    LED  blue(LED3);
+    LED  grn = LED(PTB22);
+
     for(;;) {
-        red = on;
-        blue = green = off;
-        wait(0.25);
-        blue = on;
-        green = red = off;
-        wait(0.25);
-        green = on;
-        red = blue = off;
-        wait(0.25);
+        int pause=500;
+        int n;
+        for( n=0 ; n<6 ; n++ ){
+            lights[n].on();
+            wait_ms(pause);
+            lights[n].off();
+            wait_ms(pause);
+            grn.on();
+        }
     }
+
 }
